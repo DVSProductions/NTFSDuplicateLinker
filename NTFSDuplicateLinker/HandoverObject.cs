97c776147726908665de1abb5bea43937d28cb73
DVSProductions <dvsproductions@outlook.de>
2019-01-29 22:31:34 +0100

    Extract classes into files Static tools into util.cs

---
+++ NTFSDuplicateLinker/HandoverObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace NTFSDuplicateLinker {
+	/// <summary>
+	/// Datastructure for theads to share memory
+	/// </summary>
+	struct HandoverObject {
+		public Dictionary<string, byte[]> results;
+		public Queue<WorkFile> queque;
+		public List<DuplicateFile> targets;
+	}
+}
